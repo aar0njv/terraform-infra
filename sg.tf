60d40b5da0d02826ba51b0fe4d7f766024571213
Aaron Joy <aaronjoy382@gmail.com>
2026-09-13 00:07:53 +0530

    added security group configurations

---
+++ sg.tf
@@ -0,0 +1,33 @@
+/*terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "eu-north-1"
+}*/
+
+resource "aws_security_group" "tf_server-sg" {
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = "allow http"
+  }
+  tags  = {
+    Name = "tf_server-sg"
+  }
+}
